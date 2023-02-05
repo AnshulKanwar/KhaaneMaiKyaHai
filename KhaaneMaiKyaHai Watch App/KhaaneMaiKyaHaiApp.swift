81c26bb029400d5287448c8fc8dc570c78a2283e
Anshul Kanwar <4n5hu1@protonmail.com>
2023-02-05 17:12:56 +0530

    Initial Commit

---
+++ KhaaneMaiKyaHai Watch App/KhaaneMaiKyaHaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhaaneMaiKyaHaiApp.swift
+//  KhaaneMaiKyaHai Watch App
+//
+//  Created by Anshul Kanwar on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct KhaaneMaiKyaHai_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
